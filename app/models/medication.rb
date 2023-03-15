a345e7d1e64acf131f46cad021da867ac2d7e815
Lionel Mason <capitalcookhouse@gmail.com>
2023-03-15 11:44:40 -0400

    updates README and validation in medication model

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -2,7 +2,7 @@ class Medication < ApplicationRecord
     validates :name, presence: true
     validates :form, presence: true 
     validates :instruction, presence: true, length: { in: 10..100 }
-    validates :rating, presence: true
+    validates :rating, presence: true, length: { in: 1..5}
     
     has_many :trackers
     has_many :users, through: :trackers
